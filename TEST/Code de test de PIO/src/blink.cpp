515100140569fef035ca79d4aa5564f08c819a91
Clément Boesmier <cle.chec@laposte.net>
2021-10-28 18:20:44 +0200

    fix du code de test

--- TEST/Code de test de PIO/src/blink.cpp
+++ TEST/Code de test de PIO/src/blink.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#define LED_BUILTIN 2
+
 void blink(int temps) {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(temps);
--- TEST/Code de test de PIO/src/blink.h
+++ TEST/Code de test de PIO/src/blink.h
@@ -1,2 +1,6 @@
+#ifndef BLINK_H
+#define BLINK_H
 
 void blink(int temps);
+
+#endif
