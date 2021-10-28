30d1405608f8ad618d81096de5e080f6801362c9
Clément Boesmier <cle.chec@laposte.net>
2021-10-28 20:59:54 +0200

    ajout obj

--- TEST/Code de test de PIO/src/main.cpp
+++ TEST/Code de test de PIO/src/main.cpp
@@ -1,19 +1,18 @@
 #include <Arduino.h>
-#include "blink.h"
+#include "ObjBlink.h"
 
-#define LED_BUILTIN 2
+BlinkObj led1(2, 500);
+BlinkObj led2(4, 1000);
+
+
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-bool state = 0;
-String etat = "";
 
 void loop() {
-  blink(state);
-  state = inversion(state, etat);
-  Serial.println(etat);
-  delay(1000);
+  Serial.println(led1.getEtat());
+  delay(led1.getTimer());
+  led1.actEtat();
 }
