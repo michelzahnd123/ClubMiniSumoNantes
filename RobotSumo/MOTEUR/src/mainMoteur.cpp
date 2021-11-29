e401121d65120e1e76c540c798642f4ef97c722d
Michel Zahnd <michel.zahnd123@gmail.com>
2021-11-29 19:14:50 +0100

    moteur reprise et integration de joystick

---
+++ RobotSumo/MOTEUR/src/mainMoteur.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include "moteur.h"
+#include "joystick.h"
+
+
+Moteur moteurGauche(32);
+Moteur moteurDroit(33);
+
+void Setup()
+{
+    Serial.begin(115200);
+}
+
+void Loop())
+{
+
+}
