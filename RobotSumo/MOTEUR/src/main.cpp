7653204e1eaec7f08a88ebb6bfd50da76aaac748
yann-cesbron <yann@cesbron.org>
2021-12-01 23:52:39 +0100

    Add files via upload

---
+++ RobotSumo/MOTEUR/src/main.cpp
@@ -0,0 +1,19 @@
+#include <arduino.h>
+#include <ESP32Servo.h>
+#include "moteur.h"
+#include "joystick.h"
+#include "pins.h"
+
+Moteur moteurGauche(pinMoteurGauche,0); // commande moteur en PWM
+Moteur moteurDroit(pinMoteurDroit ,0 );
+Joystick joystick(pinJoystickX, pinJoystickY, pinJoystickBT); //  X, Y, On
+
+void Setup()
+{
+    Serial.begin(115200);
+}
+
+void Loop()
+{
+   // todo
+}
