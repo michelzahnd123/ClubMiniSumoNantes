27c40bf7320da867f4738c954aee9cbaafede540
Michel Zahnd <michel.zahnd123@gmail.com>
2022-02-02 13:07:52 +0100

    Correction répertoire NOIR BLANC

---
+++ RobotSumo/lib/NoirBlanc/src/noirblanc.h
@@ -0,0 +1,15 @@
+#ifndef NOIRBLANC_H
+#define NOIRBLANC_H
+
+class NoirBlanc{
+private:
+	int pin_noirblanc;                      // pin d'entrée
+	bool noirBlanc;                         // présence à 40 cm
+	int tempsAcquisition;                   // double entrée
+public:
+	NoirBlanc(int pin_noirblanc);
+	void setPinNoirBlanc(int pin_noirblanc);
+	bool getEtatNoirBlanc();
+};
+
+#endif
